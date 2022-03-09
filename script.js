c16fde1930da14447e278e53256226b232ba9b16
NinaVarpa <88774708+NinaVarpa@users.noreply.github.com>
2022-03-09 11:01:16 +0000

    20220309

---
+++ script.js
@@ -0,0 +1,16 @@
+async function myFunction5() {
+
+      let url ='https://api.thingspeak.com/channels/1659439/feeds.json?results=1';
+        const fetchResults = await fetch(url);
+        const jsonResult = await fetchResults.json();
+        const feedsResults = jsonResult.feeds;
+       
+        for (const i in feedsResults) {
+          if(feedsResults[i].field1==1){
+          document.getElementById("vihrea").setAttribute("id", "punainen");
+          }
+          else{
+          document.getElementById("punainen").setAttribute("id", "vihrea");
+          }
+      }
+        }
